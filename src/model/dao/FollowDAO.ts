9eaa20d9451134d6be3ff9f30d71679738e366be
MichaelCh-svg <champeniosmichael@gmail.com>
2023-05-13 21:55:48 -0600

    Minor fixes to get follows pagination.

--- src/model/dao/FollowDAO.ts
+++ src/model/dao/FollowDAO.ts
@@ -7,10 +7,13 @@ export function getDAOFollowees(followerAlias: String | null, limit: number, las
     let allFollowees = fakeData.fakeUsers;
 
     let followeesIndex = lastFolloweeAlias == null ? 0 : allFollowees.findIndex(user => user.alias == lastFolloweeAlias);
+    if(followeesIndex == -1) throw Error("Follower alias " + followerAlias + " not found.");
+    
     let length = allFollowees.length;
     let remainingFolloweesCount = length - followeesIndex;
+    let returnFolloweesCount = remainingFolloweesCount > 10 ? 10 : remainingFolloweesCount;
 
-    let responseFollowees = allFollowees.splice(followeesIndex, followeesIndex + remainingFolloweesCount);
+    let responseFollowees = allFollowees.splice(followeesIndex, followeesIndex + returnFolloweesCount);
     let hasMorePages = remainingFolloweesCount > limit;
     return [responseFollowees, hasMorePages];
 }
